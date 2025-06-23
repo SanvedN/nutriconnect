386c73f0eb4de62dae51c7dfb16ab7fed14d0510
Sanved <sanvednarwadkar@gmail.com>
2025-06-23 11:32:44 +0530

    added env to ignore

--- server/config.ts
+++ server/config.ts
@@ -1,3 +1,1 @@
-export const MONGODB_URI =
-  process.env.MONGODB_URI ||
-  "mongodb+srv://admin:admin@main.it1ba.mongodb.net/nutriconnect?retryWrites=true&w=majority&appName=main";
+export const MONGODB_URI = process.env.MONGODB_URI;
